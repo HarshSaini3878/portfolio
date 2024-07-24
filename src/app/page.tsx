81f5e01024e9087923b0c1110918cf632269e6a0
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-07-25 00:17:23 +0530

    experience section created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 export default function Home() {
   return (
    <div className="overflow-clip">
    <Navbar/>
    <HeroSection/>
    <About/>
+   <Experience/>
    </div>
   );
 }
