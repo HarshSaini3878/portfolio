277441cb644dc4d676a4b060fe5f87aba34c472c
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-07-25 18:08:12 +0530

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col md:flex-row items-center justify-center gap-3 w-screen bg-green-900'>
+      <div className='w-full md:w-[45%] flex flex-col items-center justify-around bg-green-600 m-4'>
+        <div className='w-full text-2xl md:text-4xl font-bold text-white'>Work Experience </div>
+      </div>
+      <div className='w-full md:w-[45%] bg-green-600 flex flex-col m-4'>
+      <div className='w-full text-2xl md:text-4xl font-bold text-white '> Tech Stack</div>
+      <div>
+
+      </div>
+      </div>
     </div>
   )
 }
