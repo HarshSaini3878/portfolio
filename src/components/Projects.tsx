44d321baffb87a3efa9b80b7666d00a5846c778c
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-07-26 16:15:06 +0530

    projects updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
           className="w-full sm:w-1/2 p-4 flex"
         >
           <div className="w-full bg-red-400 rounded-lg p-4 flex flex-col justify-between h-full">
-            <img src={project.image} alt={project.name} className="rounded-t-lg mb-4" style={{ width: '100%', height: 'auto' }} />
+            <img src={project.image} alt={project.name} className="rounded-t-lg mb-4" style={{ width: '100%', height: '80%' }} />
             <h2 className="text-2xl font-bold mb-2">{project.name}</h2>
             <p className="mb-4">{project.description}</p>
             <h4 className="text-xl font-semibold mb-2">Technologies Used:</h4>
@@ -39,36 +39,36 @@ export default Projects
 
 const projects = [
     {
-      image: "/images/project1.jpg",
+      image: "/assets/e-commerce.png",
       name: "E-Commerce App",
       description: "Developed a dynamic eCommerce platform using the MERN stack, featuring user authentication, product listings, shopping cart, and secure checkout.",
       technologiesUsed: ["React.js", "MongoDB", "Express.js"],
       githubLink: "https://github.com/HarshSaini3878/e-commerce-app",
-      demoLink: "https://www.youtube.com/watch?v=project-one-demo"
+      demoLink: "https://youtu.be/0KS34YZwsy8"
     },
     {
-      image: "/images/project2.jpg",
+      image: "/assets/youtube (2).png",
       name: "Youtube Clone",
       description: "Built a YouTube frontend clone using React.js and Rapid API, providing video search, playback, and user interaction features.",
       technologiesUsed: ["React.js", "Api Handling", "Rapid API"],
       githubLink: "https://github.com/HarshSaini3878/youtube2.O",
-      demoLink: "https://www.youtube.com/watch?v=project-two-demo"
+      demoLink: "https://youtu.be/GoiFgaQ_o2g"
     },
     {
-      image: "/images/project3.jpg",
+      image: "/assets/music-academy.png",
       name: "Music Academy Frontend",
       description: "Created a modern Music Academy frontend using Next.js and Aceternity UI, featuring interactive course listings and seamless user experience.",
       technologiesUsed: ["Next.js", "React.js","Aceternity ui"],
       githubLink: "https://github.com/HarshSaini3878/nextjs-1st-Project",
-      demoLink: "https://www.youtube.com/watch?v=project-three-demo"
+      demoLink: "https://youtu.be/ei7aXtzgOD0"
     },
     {
-      image: "/images/project4.jpg",
+      image: "/assets/quiz.png",
       name: "KBC based Quiz app",
       description: "Designed a KBC-style quiz app frontend with React.js, featuring interactive questions, dynamic scoring, and user-friendly navigation for an engaging quiz experience.",
       technologiesUsed: ["React.js", "React hooks", "Javascript"],
       githubLink: "https://github.com/HarshSaini3878/kaun-banega-crorepati-quiz-project",
-      demoLink: "https://www.youtube.com/watch?v=project-four-demo"
+      demoLink: "https://youtu.be/zil7Yd9yleo"
     }
   ];
   
